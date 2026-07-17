56200f847fee5f57c23cdcfd98b6b89a72cfc4f7
alex2-erofeev <erofeev1607@gmail.com>
2026-07-17 14:43:13 +0300

    Создание скелета проекта
    Создал проект, настроил версии инструментов в соответствии с тз, поднял бд через docker и настроил application.yml и Liquibase

---
+++ src/main/java/ru/sfera/pm/ecommerce/PmEcommerceApplication.java
@@ -0,0 +1,13 @@
+package ru.sfera.pm.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pmecommerce/PmEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pmecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
